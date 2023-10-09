ec1490b36553ff8f118083847b51687acb03d197
Helio Monteiro <heliojr.monteiro@yahoo.com.br>
2023-10-09 10:53:47 -0300

    uso do controller

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request)
+    {
+        //return $request->get('id');
+        //return $request->getUri();
+        //return $request->method();
+        //return response('', 302, ['Location' => 'https://google.com']);
+        //return redirect('https://google.com');
+
+        $series = [
+            'Punisher',
+            'Lost',
+            'Grey\'s Anatomy'
+        ];
+
+        $html = '<ul>';
+
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+        $html .= '</ul>';
+
+        return $html;
+    }
+}
